a1f595aff72708a62a73750911db8024018e0ba0
Neil Munro <neilmunro@gmail.com>
2020-06-04 18:26:35 +0100

    Exported macros so they can actually be used!

--- src/main.lisp
+++ src/main.lisp
@@ -1,18 +1,19 @@
 (defpackage meq
-  (:use :cl))
+  (:use :cl)
+  (:export #:with-multiple-eq
+           #:with-multiple-eql
+           #:with-multiple-equal
+           #:with-multiple-equalp))
 (in-package :meq)
 
 (defmacro with-multiple-eq (&rest args)
-  `(and ,@(mapcar #'(lambda (x) (eq (first args) x)) (rest args))))
+  `(and ,@(mapcar #'(lambda (x) `(eq ,(first args) ,x)) (rest args))))
 
 (defmacro with-multiple-eql (&rest args)
-  `(and ,@(mapcar #'(lambda (x) (eql (first args) x)) (rest args))))
+  `(and ,@(mapcar #'(lambda (x) `(eql ,(first args) ,x)) (rest args))))
 
 (defmacro with-multiple-equal (&rest args)
-  `(and ,@(mapcar #'(lambda (x) (equal (first args) x)) (rest args))))
+  `(and ,@(mapcar #'(lambda (x) `(equal ,(first args) ,x)) (rest args))))
 
 (defmacro with-multiple-equalp (&rest args)
-  `(and ,@(mapcar #'(lambda (x) (equalp (first args) x)) (rest args))))
-
-(with-multiple-eq 1 1 1)
-(with-multiple-eq 2 1 1)
+  `(and ,@(mapcar #'(lambda (x) `(equalp ,(first args) ,x)) (rest args))))
